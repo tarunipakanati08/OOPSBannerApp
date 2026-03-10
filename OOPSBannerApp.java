72d285b10b32ed9c122c4483fcf80cba9ecb553a
tarunipakanati08 <taruni1308@gmail.com>
2026-03-11 00:56:35 +0530

    added uc2

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,7 +1,15 @@
 public class OOPSBannerApp
-{
-    public static void main(String[]args)
-{
-    System.out.println("OOPS");
-}
+ {
+    public static void main(String[] args) {
+
+        System.out.println("   ***        ***      ******        **** ");
+        System.out.println(" **   **   **     **   **    **    **   ");
+        System.out.println("**     **  **     **   **     **  **     ");
+        System.out.println("**     **  **     **   **    **    **");
+        System.out.println("**     **  **     **   ******        ***  ");
+        System.out.println("**     **  **     **   **               **");
+        System.out.println("**     **  **     **   **                **");
+        System.out.println(" **   **    **   **    **               **");
+        System.out.println("   ***        ***      **           **** ");
+    }
 }
